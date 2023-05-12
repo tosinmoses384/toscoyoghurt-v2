6bd750157d506899044121c5d3382a06bba7d88c
Israel <tosinmoses384@gmail.com>
2023-05-12 20:34:34 +0100

    mongoDB atlas setup mongodb compass setup connect with mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import products from './data/products.js';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 
 const port = process.env.PORT || 5000;
 dotenv.config();
+connectDB();
 
 const app = express();
 
