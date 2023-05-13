066541e230444f19a6f89abb175e6b123de4a0a5
Israel <tosinmoses384@gmail.com>
2023-05-13 14:03:25 +0100

    product controller

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,23 @@
-import express from 'express';
-// import products from './data/products.js';
-import dotenv from 'dotenv';
-import connectDB from './config/db.js';
-import productRoutes from './routes/productRoutes.js';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 const port = process.env.PORT || 5000;
 dotenv.config();
 connectDB();
 
-
 const app = express();
 
-
-
-app.get('/', (req, res) => {
-  res.send('API is running...');
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
 
-app.use('/api/products', productRoutes);
+app.use("/api/products", productRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-
-app.listen(port, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`));
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
